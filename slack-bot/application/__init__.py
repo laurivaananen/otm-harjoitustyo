22f4602ae5a46c33a9d26f609a49ba683176b7af
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-22 21:00:59 +0300

    First gui

--- slack-bot/application/__init__.py
+++ slack-bot/application/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from application import views
 import os
 
 if "BOT_OAUTH" not in os.environ.keys():
@@ -9,3 +8,5 @@ if "SLACK_OAUTH" not in os.environ.keys():
     print("\n\nPLEASE SET SLACK_OAUTH ENVIRONMENT VARIABLE CORRECTLY\n\n")
 
 app = Flask(__name__)
+
+from application import views
--- slack-bot/run.py
+++ slack-bot/run.py
@@ -1,4 +1,51 @@
-from application import app 
+from application import app
+from PyQt5.QtCore import QThread, QObject
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QPushButton
+import sys
+
+class ServerThread(QThread):
+
+    def __init__(self, app):
+        QThread.__init__(self)
+        self.app = app
+
+    def run(self):
+        self.app.run(debug=False, port=3000)
+
+class MainGui(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        # QMainWindow.__init__(self)
+
+        self.thread = ServerThread(app)
+        self.thread.start()
+
+        self.initUi()
+
+    def initUi(self):
+        button = QPushButton("Hello")
+        vbox = QVBoxLayout()
+        # vbox.addStretch(1)
+        vbox.addWidget(button)
+
+        self.setLayout(vbox)
+        self.setGeometry(0, 0, 800, 600)
+        self.setWindowTitle('Slack Bot')    
+        self.show()
+
+
 
 if __name__ == '__main__':
-    app.run(debug=False, port=3000)
+    gui = QApplication(sys.argv)
+    w = MainGui()
+    # w.show()
+    sys.exit(gui.exec_())
+
+    
+
+    
+
+    
